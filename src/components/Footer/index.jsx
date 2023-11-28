4e552926c2e72bbd6227773c680e6de65e25023b
frontend-case <frontend-case@mail.ru>
2023-11-28 19:35:08 +0500

    Home

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,7 +1,39 @@
-import React from 'react'
+import React from 'react';
+import style from './Footer.module.css'
 
 export default function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <>
+      <footer className="bg-[#161616] mt-[50px] px-[15px]">
+        <div className='flex place-content-around pt-[30px] pb-[20px] border-b-[1px] border-[#333232]'>
+          <ul className='flex flex-col gap-[8px]'>
+            <li>PLATFORM</li>
+            <li><a href="#">Support</a> </li>
+            <li><a href="#">FAQ</a></li>
+            <li><a href="#">Partnerships</a></li>
+            <li><a href="#">Blog</a></li>
+          </ul>
+
+          <ul className='flex flex-col gap-[8px]'>
+            <li>ABOUT US</li>
+            <li><a href="#">Terms & Conditions</a></li>
+            <li><a href="#">AML Policy</a></li>
+            <li><a href="#">Responsibility</a></li>
+            <li><a href="#">Privacy Policy</a></li>
+          </ul>
+
+          <ul className='flex flex-col gap-[8px]'>
+            <li>COMMUNITY</li>
+            <li><a href="#">Telegram</a></li>
+            <li><a href="#"> X (Twitter) </a></li>
+            <li><a href="#">Discord</a></li>
+          </ul>
+        </div>
+
+        <div className='pt-[20px] pb-[50px]'>
+          <img src="./img/icons/logo/Logo.png" alt="logo" />
+        </div>
+      </footer>
+    </>
+  );
 }
