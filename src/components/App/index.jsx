83be0dfd5fe792b45b31999427199b4bf0cb6e44
Maxim Khanukaev <Andreymaxim387@gmail.com>
2023-11-28 18:10:48 +0300

    Add router dom

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,26 +1,22 @@
 import React from 'react';
 import './App.css';
-// cjjd
-import Header from '../Header';
-import Category from '../Category';
-import BannerSale from '../MainBanner/Sale';
-import BannerNewCol from '../MainBanner/NewCol';
-import Footer from '../Footer';
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
+import Layout from '../Layout/Layout';
+import Error from '../Error';
 
-function App() {
+export default function index() {
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/' element={<Layout />} errorElement={<Error />}>
+        <Route index element={<Home/>}/>
+      </Route>
+    )
+  );
+
   return (
     <>
-      <Header />
-      <div className="wrapper max-w-[1270px] m-auto px-[35px]">
-        <Category />
-        <div className={`banners flex gap-[30px] justify-center`}>
-            <BannerSale />
-            <BannerNewCol />
-        </div>
-      </div>
-      <Footer />
+      <RouterProvider router = {router}/>
     </>
-  );
+  )
 }
-
-export default App;
